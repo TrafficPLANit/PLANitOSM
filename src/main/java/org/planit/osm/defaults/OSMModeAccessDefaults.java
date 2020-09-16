5bc98fac805ca1eaa09fe1b2ab5addb24bfc97b8
Mark Raadsen <mark.raadsen@sydney.edu.au>
2020-09-16 12:27:29 +1000

    working on mode features to better support import/export conversions. NOT DONE

---
+++ src/main/java/org/planit/osm/defaults/OSMModeAccessDefaults.java
@@ -0,0 +1,11 @@
+package org.planit.osm.defaults;
+
+/**
+ * The defaults for the mode access in case no specific restrictions are indicated on the highway=type way.
+ * 
+ * @author markr
+ *
+ */
+public class OSMModeAccessDefaults {
+
+}
